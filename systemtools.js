016aac7ef136462a583cafbcc88cfe214b9d710f
DownUp Studio <downwiththehonestman@gmail.com>
2026-01-21 18:49:16 +0100

    1.0

---
+++ systemtools.js
@@ -0,0 +1,42 @@
+const toolsData = {
+    "system_tools": [
+        {
+            "name": "SuperF4",
+            "description": "Kill programs instantly with Ctrl+Alt+F4.",
+            "version": "1.4",
+            "size": "138 KB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://github.com/stefansundin/superf4/releases/download/v1.4/SuperF4-1.4.exe",
+            "download_text": "Download the app",
+            "icon": "https://stefansundin.github.io/superf4/img/icon128.png",
+            "details": "SuperF4 kills the foreground program when you press Ctrl+Alt+F4. This is different from when you press Alt+F4. When you press Alt+F4, the program can refuse to quit. Windows only asks the program to quit, and lets it decide for itself what to do."
+        },
+        {
+            "name": "Bulk Crap Uninstaller",
+            "description": "Remove large amounts of applications with minimal to no user input",
+            "version": "5.9",
+            "size": "7,8 MB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://github.com/Klocman/Bulk-Crap-Uninstaller/releases/download/v5.9/BCUninstaller_5.9.0_setup.exe",
+            "download_text": "Download the app",
+            "icon": "https://img.utdstc.com/icon/c8a/247/c8a2479faaa76ad5a87b76e07881570260d5647e36dcfe887a720234208e13d4:200",
+            "details": "Bulk Crap Uninstaller (in short BCUninstaller or BCU) is a free (as in speech and beer) bulk program uninstaller with advanced automation. It excels at removing large amounts of applications with minimal to no user input, while requiring next to no technical knowledge."
+        },
+      {
+            "name": "PolicyPlus",
+            "description": "Local Group Policy Editor plus more, for all Windows editions.",
+            "version": "October 2025 Snapshot",
+            "size": "364 KB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://github.com/Fleex255/PolicyPlus/releases/download/October2025/PolicyPlus.exe",
+            "download_text": "Download the app",
+            "details": "Policy Plus is a local Group Policy Editor for all versions of Windows. It allows you to view and edit policy settings on computers that don't have Group Policy Editor installed, or to manage policy settings remotely."
+        }
+    ]
+};
