1f21db08b4f7ac41165b4f1b870f7d5b6342cdd2
DownUp Studio <downwiththehonestman@gmail.com>
2026-01-23 14:42:42 +0100

    Update systemtools.js

--- systemtools.js
+++ systemtools.js
@@ -89,5 +89,18 @@ const toolsData = {
             "icon": "https://www.hwinfo.com/wp-content/themes/hwinfo/img/logo-sm.png",
             "details": "HWiNFO is a system information and hardware monitoring tool that provides detailed information about your computer's hardware components."
         },
+        {
+            "name": "Monitorian",
+            "description": "A Windows desktop tool to adjust the brightness of multiple monitors with ease.",
+            "version": "4.13.0",
+            "size": "629 KB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://github.com/emoacht/Monitorian/releases/download/4.13.0-Installer/MonitorianInstaller4130.zip",
+            "download_text": "Download the app",
+            "icon": "https://store-images.s-microsoft.com/image/apps.34982.14453285281266867.f7b19530-4829-4646-9985-b7e1e716c416.2a3d0398-0d33-46a3-b739-47b31af65e59?h=210",
+            "details": "Monitorian is a Windows desktop tool to adjust the brightness of multiple monitors with ease."
+        },
     ]
 };
