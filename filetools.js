bbf00d3fd5ad7b32999721450e6a3b3f8696c6fe
DownUp Studio <downwiththehonestman@gmail.com>
2026-01-24 20:34:32 +0100

    File Tools added

---
+++ filetools.js
@@ -0,0 +1,17 @@
+const toolsData = {
+    "file_tools": [
+        {
+            "name": "Free Download Manager",
+            "description": "A powerful download manager for Windows",
+            "version": "6.33",
+            "size": "46,4 MB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://files2.freedownloadmanager.org/6/latest/fdm_x64_setup.exe",
+            "download_text": "Download the app",
+            "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHM0OYfiFeMI2p9MWie0CvL99U4GA1gf6_kayTt_kBblFwHwo8BW8JXlqfnYxKPmmBf5ZnXnMl_rd760sCzKS9ENz20qYyOVIE9TfI7WT5WxOgJwtxsqAOGlG5J.uloKJCH348TrrzTWBtLiVsJ9jGSI-&format=source",
+            "details": "Free Download Manager is a powerful download manager for Windows. It allows you to adjust traffic usage, organize downloads, control file priorities for torrents, efficiently download large files and resume broken downloads."
+        },
+    ]
+};
