b4feb606cf47a674512d6d2b9d82bc920b366c4f
DownUp Studio <downwiththehonestman@gmail.com>
2026-01-24 20:42:55 +0100

    Update filetools.js

--- filetools.js
+++ filetools.js
@@ -2,7 +2,7 @@ const toolsData = {
     "file_tools": [
         {
             "name": "Free Download Manager",
-            "description": "A powerful download manager for Windows",
+            "description": "A powerful download manager for Windows.",
             "version": "6.33",
             "size": "46,4 MB",
             "platform": "Windows",
@@ -12,6 +12,32 @@ const toolsData = {
             "download_text": "Download the app",
             "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHM0OYfiFeMI2p9MWie0CvL99U4GA1gf6_kayTt_kBblFwHwo8BW8JXlqfnYxKPmmBf5ZnXnMl_rd760sCzKS9ENz20qYyOVIE9TfI7WT5WxOgJwtxsqAOGlG5J.uloKJCH348TrrzTWBtLiVsJ9jGSI-&format=source",
             "details": "Free Download Manager is a powerful download manager for Windows. It allows you to adjust traffic usage, organize downloads, control file priorities for torrents, efficiently download large files and resume broken downloads."
+        },
+      {
+            "name": "WinRAR",
+            "description": "Compress, encrypt, package and backup with only one utility.",
+            "version": "7.13",
+            "size": "4 MB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-713.exe",
+            "download_text": "Download the app",
+            "icon": "https://i.wpimg.pl/1000x/filerepo.grupawp.pl/api/v1/display/embed/d5d4029c-d6b2-428f-af3f-b55b3a9ad967",
+            "details": "WinRAR is a powerful archive manager. It can backup your data and reduce the size of email attachments, decompress RAR, ZIP and other files downloaded from the Internet and create new archives in RAR and ZIP file format."
+        },
+        {
+            "name": "File Converter",
+            "description": "Convert files directly from the context menu.",
+            "version": "2.1",
+            "size": "54 MB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://github.com/Tichau/FileConverter/releases/download/v2.1/FileConverter-2.1-x64-setup.msi",
+            "download_text": "Download the app",
+            "icon": "https://file-converter.io/images/application-icon.png",
+            "details": "File Converter is a utility that allows you to convert files directly from the context menu. It supports a wide range of formats and can be used to convert audio, video, images, and documents."
         },
     ]
 };
