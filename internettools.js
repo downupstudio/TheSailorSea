2e72ee1d0e6d1306bf4678d45b95282a139b4909
DownUp Studio <downwiththehonestman@gmail.com>
2026-02-02 22:54:23 +0100

    Update internettools.js

--- internettools.js
+++ internettools.js
@@ -13,5 +13,17 @@ const toolsData = {
             "icon": "https://www.softexia.com/wp-content/uploads/2025/03/KeePass_logo.webp",
             "details": "KeePass is a free, open-source password manager for Windows. It allows you to store and manage passwords securely."
         },
+       {
+            "name": "Bypass Paywalls Clean",
+            "description": "A browser extension that bypasses paywalls on news websites.",
+            "version": "4.7.0",
+            "size": "4 MB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass-paywalls-chrome-clean-latest.crx&inline=false&commit=c08e72e378d9e4a6d0d8b4dd21cc649d4a169f49",
+            "download_text": "Download the extension",
+            "details": "Bypass Paywalls Clean is a browser extension that removes paywalls from news websites. It supports a wide range of sites and is easy to use."
+        },
     ]
 };
