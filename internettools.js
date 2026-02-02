0a91814a61c97c26c78eaecb8e536cdc7bbac2c6
DownUp Studio <downwiththehonestman@gmail.com>
2026-02-02 22:52:00 +0100

    net tools

---
+++ internettools.js
@@ -0,0 +1,17 @@
+const toolsData = {
+    "internet_tools": [
+        {
+            "name": "KeePass",
+            "description": "A powerful password manager for Windows.",
+            "version": "2.60",
+            "size": "4 MB",
+            "platform": "Windows",
+            "status": "Downloadable",
+            "status_color": "#b39dff",
+            "download_url": "https://unlimited.dl.sourceforge.net/project/keepass/KeePass 2.x/2.60/KeePass-2.60-Setup.exe?viasf=1",
+            "download_text": "Download the app",
+            "icon": "https://www.softexia.com/wp-content/uploads/2025/03/KeePass_logo.webp",
+            "details": "KeePass is a free, open-source password manager for Windows. It allows you to store and manage passwords securely."
+        },
+    ]
+};
